cdd7b24d97ae2dab0282ec36cd3bab893fe52a3c
YangYoungYoung <1249091287@qq.com>
2019-03-19 20:42:14 +0800

    增加了获取商品列表接口

--- pages/commodity/commodity.js
+++ pages/commodity/commodity.js
@@ -7,7 +7,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    shopList: []
+    shopList: [],
+    selectList: [],
+    userId: ''
   },
 
   /**
@@ -16,7 +18,9 @@ Page({
   onLoad: function(options) {
     let that = this;
     let userId = wx.getStorageSync('userId');
-
+    that.setData({
+      userId: userId
+    })
     let url = "goods/list?userId=" + userId
     var params = {
 
@@ -35,8 +39,8 @@ Page({
             that.setData({
               shopList: shopList
             })
-          }else{
-            common.showTip("暂无数据",'loading');
+          } else {
+            common.showTip("暂无数据", 'loading');
           }
 
         } else {
@@ -65,5 +69,76 @@ Page({
 
   },
 
+  //选择商品
+  onSelect: function(event) {
+    let that = this;
+    let index = event.currentTarget.dataset.index;
+    let shopList = that.data.shopList;
+    shopList[index].select = true;
+    that.setData({
+      shopList: shopList
+    })
+    selectList.push(shopList[index]);
+  },
+  //提交
+  submit: function() {
+    let that = this;
+    let userId = that.data.userId;
+    let selectList = that.data.selectList;
+    let goodsIds = '';
+    for (var i = 0; i < selectList.length; i++) {
+      if (selectList.hasNext) {
+        goodsIds += selectList[i] + ','
+      } else {
+        goodsIds += selectList[i]
+      }
+    }
+
+    let url = "zhiBo/room/" + userId + "/goodsIds?goodsIds=" + goodsIds
+    var params = {
+
+    }
+    let method = "PUT";
+    wx.showLoading({
+        title: '加载中...',
+      }),
+      network.POST(url, params, method).then((res) => {
+        wx.hideLoading();
+        if (res.data.code == 200) {
+          common.showTip("提交成功", 'success');
+
+          // console.log(res.data.data.goodsS);
+          // let shopList = res.data.data.goodsS
+          // if (shopList.length != 0) {
+          //   that.setData({
+          //     shopList: shopList
+          //   })
+          // } else {
+          //   common.showTip("暂无数据", 'loading');
+          // }
+
+        } else {
+          wx.showToast({
+            title: '网络错误',
+            icon: 'loading',
+            duration: 1500,
+          })
+        }
+
+      }).catch((errMsg) => {
+        wx.hideLoading();
+        console.log(errMsg); //错误提示信息
+        wx.showToast({
+          title: '网络错误',
+          icon: 'loading',
+          duration: 1500,
+        })
+      });
+  },
+  toSearch:function(){
+    wx.navigateTo({
+      url: '../search/search',
+    })
+  }
 
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    searchText: ''
   },
 
   /**
@@ -22,11 +22,33 @@ Page({
   onShow: function() {
 
   },
-  onSearch: function() {
+  //搜索功能
+  onSearch: function(event) {
+    // console.log('搜索功能', event.detail);
+    let that = this;
+    let searchText = that.data.value;
+    console.log('搜索功能', searchText);
+    that.setData({
+      searchText: event.detail
+    })
+    if (searchText != '' && searchText != null) {
+      that.search();
+    }
+
+  },
+  onChange: function(event) {
+    let that = this;
+    let searchText = event.detail;
+    that.setData({
+      value: searchText
+    })
+  },
+  search: function() {
     let that = this;
     let userId = wx.getStorageSync('userId');
+    let searchText = that.data.searchText;
 
-    let url = "goods/list?userId=" + userId
+    let url = "goods/list?userId=" + userId + "&searchText=" + searchText;
     var params = {
 
     }
@@ -65,6 +87,60 @@ Page({
           duration: 1500,
         })
       });
-  }
+  },
+  //提交
+  submit: function() {
+    let that = this;
+    let userId = that.data.userId;
+    let selectList = that.data.selectList;
+    let goodsIds = '';
+    for (var i = 0; i < selectList.length; i++) {
+      if (selectList.hasNext) {
+        goodsIds += selectList[i] + ','
+      } else {
+        goodsIds += selectList[i]
+      }
+    }
 
+    let url = "zhiBo/room/" + userId + "/goodsIds?goodsIds=" + goodsIds
+    var params = {
+
+    }
+    let method = "PUT";
+    wx.showLoading({
+        title: '加载中...',
+      }),
+      network.POST(url, params, method).then((res) => {
+        wx.hideLoading();
+        if (res.data.code == 200) {
+          common.showTip("提交成功", 'success');
+
+          // console.log(res.data.data.goodsS);
+          // let shopList = res.data.data.goodsS
+          // if (shopList.length != 0) {
+          //   that.setData({
+          //     shopList: shopList
+          //   })
+          // } else {
+          //   common.showTip("暂无数据", 'loading');
+          // }
+
+        } else {
+          wx.showToast({
+            title: '网络错误',
+            icon: 'loading',
+            duration: 1500,
+          })
+        }
+
+      }).catch((errMsg) => {
+        wx.hideLoading();
+        console.log(errMsg); //错误提示信息
+        wx.showToast({
+          title: '网络错误',
+          icon: 'loading',
+          duration: 1500,
+        })
+      });
+  }
 })
